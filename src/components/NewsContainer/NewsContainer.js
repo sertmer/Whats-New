40711544740aff662a4f314f398c8c0a759c4b44
sertmer <scottertmer@gmail.com>
2019-11-19 08:33:08 -0700

    Build NewsContainer Component

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import './NewsContainer.css'
+import NewsArticle from '../NewsArticle/NewsArticle';
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = ({data}) => {
+  let articles = data.map(story => {
+  let {id, headline, description, img} = story
+    return (
+      <NewsArticle 
+        key={id} 
+        headline={headline} 
+        // description={description}
+        // img={img}
+      />
+    )
+  })
+  return (
+    <section>
+      {articles}
+    </section>
+  )
+}
 
 export default NewsContainer;
