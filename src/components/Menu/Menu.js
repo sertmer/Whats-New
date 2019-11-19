5c953b7e09688e08f11ce3d20fb4f04de7eef032
sertmer <scottertmer@gmail.com>
2019-11-19 13:00:20 -0700

    Build initial Menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = () => {
+  return (
+    <aside>
+      <h1>What's <span>New?</span></h1>
+      <button>Local News</button>
+      <button>Technology</button>
+      <button>Entertainment</button>
+      <button>Science</button>
+      <button>Health</button>
+    </aside>
+  )
+}
 
 export default Menu;
