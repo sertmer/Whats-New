94e1759a333dbc782f87997723e422b568d6a2d6
sertmer <scottertmer@gmail.com>
2019-11-19 13:00:05 -0700

    Import data, store in App state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment'
+import health from '../..data/health'
+import science from '../..data/science'
+import technology from '../..data/technology'
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer'
 
@@ -7,13 +11,18 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local,
+      entertainment,
+      health,
+      science,
+      technology
     }
   }
 
   render () {
     return (
       <div className="app">
+        <Menu />
         <NewsContainer data={this.state.local}/>
       </div>
     );
