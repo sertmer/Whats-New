27b824394ef5e5102f515590e8f3cbbfb9d90cc8
sertmer <scottertmer@gmail.com>
2019-11-19 13:24:43 -0700

    Add className to buttons

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,13 +3,13 @@ import './Menu.css'
 
 const Menu = () => {
   return (
-    <aside>
+    <aside className='menu'>
       <h1>What's <span>New?</span></h1>
-      <button>Local News</button>
-      <button>Technology</button>
-      <button>Entertainment</button>
-      <button>Science</button>
-      <button>Health</button>
+      <button className='menu-btn'>Local News</button>
+      <button className='menu-btn'>Technology</button>
+      <button className='menu-btn'>Entertainment</button>
+      <button className='menu-btn'>Science</button>
+      <button className='menu-btn'>Health</button>
     </aside>
   )
 }
