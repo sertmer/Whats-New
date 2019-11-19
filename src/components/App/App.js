b7813508176fbd81b8b58723e83098e8596f8f7f
sertmer <scottertmer@gmail.com>
2019-11-19 13:06:46 -0700

    Correct file path, add import for menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import entertainment from '../../data/entertainment'
-import health from '../..data/health'
-import science from '../..data/science'
-import technology from '../..data/technology'
+import health from '../../data/health'
+import science from '../../data/science'
+import technology from '../../data/technology'
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer'
+import Menu from '../Menu/Menu'
 
 class App extends Component {
   constructor() {
