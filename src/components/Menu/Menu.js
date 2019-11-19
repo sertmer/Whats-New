6f081a9cd7aaedabf79d8a902c8d59702e75c401
sertmer <scottertmer@gmail.com>
2019-11-19 14:17:12 -0700

    Add changeCurrentPage function to button onClick

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import './Menu.css'
+import local from '../../data/local';
+import entertainment from '../../data/entertainment'
+import health from '../../data/health'
+import science from '../../data/science'
+import technology from '../../data/technology'
 
-const Menu = () => {
+const Menu = ({ changeCurrentPage }) => {
   return (
     <aside className='menu'>
       <h1>What's <span>New?</span></h1>
-      <button className='menu-btn'>Local News</button>
-      <button className='menu-btn'>Technology</button>
-      <button className='menu-btn'>Entertainment</button>
-      <button className='menu-btn'>Science</button>
-      <button className='menu-btn'>Health</button>
+      <button 
+        className='menu-btn' 
+        onClick={() => changeCurrentPage(local)}>
+        Local News
+      </button>
+      <button 
+        className='menu-btn' 
+        onClick={() => changeCurrentPage(technology)}>
+        Technology
+      </button>
+      <button 
+        className='menu-btn' 
+        onClick={() => changeCurrentPage(entertainment)}>
+        Entertainment
+      </button>
+      <button 
+        className='menu-btn' 
+        onClick={() => changeCurrentPage(science)}>
+        Science
+      </button>
+      <button 
+        className='menu-btn' 
+        onClick={() => changeCurrentPage(health)}>
+        Health
+      </button>
     </aside>
   )
 }
