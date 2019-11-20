e9c5c41ef219a3922f10310aca8000d85c1fdff4
sertmer <scottertmer@gmail.com>
2019-11-20 07:35:25 -0700

    Convert SearchForm into class component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-const SearchForm = (props) => {
-  return (
+class SearchForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      search: '',
+    }
+  }
+  
+  searchCurrentPage = (query) => {
+    const capitalQuery = query.toUpperCase()
+    console.log(capitalQuery);
+    this.state.currentPage.filter(story => {
+      return story.title.contains(capitalQuery)
+    })
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  render() { 
+    return (
     <nav className='nav'>
       <h1>What's <span> New? </span></h1>
       <input 
+        name='search'
+        value={this.state.search}
         type='text' 
         placeholder='Search for news here'
-        className='search-input'>
+        className='search-input'
+        onChange={this.handleChange}>
       </input>
-      <button className='search-submit'>Search</button>
+      <button className='search-submit' onClick={() => this.searchCurrentPage(this.state.search)}>Search</button>
     </nav>
-  )
+  )}
 }
 export default SearchForm;
