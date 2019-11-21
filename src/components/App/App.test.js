56999b29656134ca258f4a97915af069a827d09d
sertmer <scottertmer@gmail.com>
2019-11-21 13:37:05 -0700

    Add test for changeCurrentPage

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,8 +3,42 @@ import App from './App';
 import { shallow } from 'enzyme'
 
 
-it('should match the snapshot with all data passed in correctly', () => {
-  const wrapper = shallow(<App />)
+describe('App', () => {
+  let wrapper
 
-  expect(wrapper).toMatchSnapshot()
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('should match the snapshot with all data passed in correctly', () => {   
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('Should update state when changeCurrentPage is invoked', () => {
+    const mockEntertainment = [{
+      id: 1,
+      headline: 'string',
+      img: 'string',
+      url: 'string', 
+      description: 'string'
+    }]
+    const mockLocal = [{
+      id: 1,
+      headline: 'string',
+      img: 'string',
+      url: 'string', 
+      description: 'string'
+    }]
+    const expected = {currentPage: mockEntertainment}
+    const defaultState = {currentPage: mockLocal}
+
+    wrapper.setState(defaultState)
+    expect(wrapper.state()).toEqual(defaultState)
+
+    wrapper.instance().changeCurrentPage(mockEntertainment)
+    expect(wrapper.state()).toEqual(expected)
+  })
+
 })
+
+
