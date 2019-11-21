c3eb0161568e121559bb7071a901eb8d19b0fbb7
sertmer <scottertmer@gmail.com>
2019-11-21 15:58:31 -0700

    Add test for handleChange

--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -3,11 +3,26 @@ import SearchForm from './SearchForm'
 import { shallow } from 'enzyme'
 
 describe('SearchForm', () => {
-  it('should match the snapshot with all data passed in correctly', () => {
-    const wrapper = shallow(<SearchForm 
-      searchCurrentPage={jest.fn()}
+  let wrapper
+  let mockSearchCurrentPage = jest.fn()
+
+  beforeEach(() => {
+    wrapper = shallow(<SearchForm 
+      searchCurrentPage={mockSearchCurrentPage}
       />)
-    
+  })
+
+  it('should match the snapshot with all data passed in correctly', () => {  
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should update state when handleChange is invoked', () => {
+    const mockEvent = {target: {name: 'search', value: 'search query'}}
+
+    expect(wrapper.state('search')).toEqual('')
+
+    wrapper.instance().handleChange(mockEvent)
+    
+    expect(wrapper.state('search')).toEqual('search query')
   })
 })
