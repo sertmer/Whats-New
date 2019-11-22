ca529db07a43c773608869de153739ed31619495
sertmer <scottertmer@gmail.com>
2019-11-22 08:34:30 -0700

    Add fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
-import local from '../../data/local';
-import entertainment from '../../data/entertainment'
-import health from '../../data/health'
-import science from '../../data/science'
-import technology from '../../data/technology'
+// import local from '../../data/local';
+// import entertainment from '../../data/entertainment'
+// import health from '../../data/health'
+// import science from '../../data/science'
+// import technology from '../../data/technology'
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer'
 import Menu from '../Menu/Menu'
 import SearchForm from '../SearchForm/SearchForm'
 
+let local
+let entertainment
+let health
+let science
+let technology
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-       currentPage: local
+       currentPage: []
     }
   }
   
+  componentDidMount() {
+    fetch('https://whats-new-api.herokuapp.com/api/v1/news')
+      .then(res => res.json())
+      .then(newsData => this.assignData(newsData))
+      .catch(err => console.log(err))
+  }
+
+  assignData = (data) => {
+    local = data.local
+    entertainment = data.entertainment
+    health = data.health
+    science = data.science
+    technology = data.technology
+    this.setState({currentPage: local})
+  }
+
   changeCurrentPage = (type) => {
     this.setState({currentPage: type})
   }
